4d9b20ea7a261e3ba4ce17d12db5b799cf6b1a13
lisaw97 <lisawen0614@gmail.com>
2020-05-14 15:08:03 -0700

    Set up api and search bar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const APP_ID = 'e873212f';
+  const APP_KEY = '8d3190c4d6d004b3cea5acc4c1e64e4e';
+
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">Search</button>
+      </form>
     </div>
   );
 }
