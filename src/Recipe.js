e03875500ebf23761f4314cf86597b7459ba644c
lisaw97 <lisawen0614@gmail.com>
2020-05-14 21:26:18 -0700

    Create search function

--- src/Recipe.js
+++ src/Recipe.js
@@ -5,7 +5,7 @@ const Recipe = ({ title, calories, image }) => {
     <div>
       <h1>{ title }</h1>
       <p>{ calories }</p>
-      <img src={ image } />
+      <img src={ image } alt=""/>
     </div>
   );
 }
