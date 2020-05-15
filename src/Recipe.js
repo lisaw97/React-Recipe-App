02bfe6e28a45f48d6b50e3dfb421e1a2d8562ea1
lisaw97 <lisawen0614@gmail.com>
2020-05-14 21:53:07 -0700

    Update styling

--- src/Recipe.js
+++ src/Recipe.js
@@ -7,11 +7,11 @@ const Recipe = ({ title, calories, image, ingredients }) => {
       <h1>{ title }</h1>
       <ol>
         { ingredients.map(ingredient => (
-          <li>{ ingredient.text }</li>
-        ))}
+          <li>{ingredient.text}</li>
+        )) }
       </ol>
       <p>{ calories }</p>
-      <img src={ image } alt=""/>
+      <img className={ style.image } src={ image } alt=""/>
     </div>
   );
 }
